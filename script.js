49590d573fc8c60f87bb57cf58ceefc5831a6c56
CryoXen <al222011226@gmail.com>
2025-09-18 10:01:28 -0600

    solución

---
+++ script.js
@@ -0,0 +1,12 @@
+function mensajeMagico() {
+    alert ("ES HORA DE UNA HAMBURGUESA")
+}
+
+function mensajeSecreto() {
+    console.log("Alguien ha hecho click en BURGER TOWN")
+}
+
+function cambiarColor() {
+  const burger = document.getElementById("burgerText");
+  burger.style.color = "red";
+}
